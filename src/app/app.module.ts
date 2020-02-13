06ed925ddc9103a97b447698b41e64483f8dbd2e
lanxuexing <lanxuexing313wsr@163.com>
2020-02-14 02:50:25 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { TreeModule } from 'angular-tree-component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
